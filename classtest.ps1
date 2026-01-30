1907051d1e8c9ba8e8d1088f5e8a4215362332fc
javiersanabria-tech <javier.sanabria@ht-la.org>
2026-01-29 22:37:18 -0800

    Update and rename classtest.ps1 to EMPTY

--- classtest.ps1
+++
@@ -1,5 +0,0 @@
-$uri = "https://github.com/soulcurrymedia/twitter-emotion-recognition/blob/master/models/allowed-chars.pkl"
-
-$output = ".\allowed-chars.pkl"
-
-Invoke-WebRequest -Uri $uri -outfile $output
